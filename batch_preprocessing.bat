8c5e500609b9f5dbf1ba14fa353ecf013fea9fca
tlyi <t.leyi2305@gmail.com>
2023-05-21 16:24:55 +0200

    alter bash commands

--- batch_preprocessing.bat
+++ batch_preprocessing.bat
@@ -15,3 +15,4 @@ python3 -m openpifpaf_openlane.openlane_to_coco \
     --dir_data='/work/scitas-share/datasets/Vita/civil-459/OpenLane/raw/lane3d_300' \
     --dir_images='/work/scitas-share/datasets/Vita/civil-459/OpenLane/raw/images' \
     --dir_out='./data_openlane' 
+    --sample 
